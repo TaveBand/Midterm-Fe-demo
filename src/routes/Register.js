8eeecc9a16267b0545f18e3e974376f0dce14568
YeongSeon Yun <dudtjswnstjscoding@gmail.com>
2024-07-05 23:07:23 +0900

    Merge: yys into main

--- src/routes/Register.js
+++ src/routes/Register.js
@@ -2,29 +2,6 @@ import React, { useState } from "react";
 import Header from "../components/Header";
 import { useNavigate } from "react-router-dom";
 import instance from "./axios";
-<<<<<<< HEAD
-
-import "./Register.css";
-
-function Register() {
-  const [id, setId] = useState("");
-  const [nickname, setNickname] = useState("");
-  const [password, setPassword] = useState("");
-  const [checkpassword, setCheckpassword] = useState("");
-  const navigate = useNavigate();
-
-  const handleIDChange = (e) => {
-    setId(e.target.value);
-  };
-  const handleNicknameChange = (e) => {
-    setNickname(e.target.value);
-  };
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-  const handleCheckChange = (e) => {
-    setCheckpassword(e.target.value);
-=======
 import "./Register.css";
 
 function Register() {
@@ -48,23 +25,10 @@ function Register() {
 
   const handleCheckChange = (e) => {
     setCheckPassword(e.target.value);
->>>>>>> yys
   };
 
   // 이메일 인증 페이지로 이동하는 함수
   const goToEmailVerification = () => {
-<<<<<<< HEAD
-    navigate("/register/verify"); // "/email-verification"는 이메일 인증 페이지의 경로입니다. 실제 경로에 맞게 수정해주세요.
-  };
-  // 회원가입 페이지 아이디 닉네임 비번 get post 추가
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const userinfo = {
-      id : id,
-      nickname: nickname,
-      password:password,
-      checkpassword:checkpassword,
-=======
     if (password !== checkPassword) {
       alert("비밀번호가 일치하지 않습니다.");
       return;
@@ -81,17 +45,12 @@ function Register() {
       username,
       nickname,
       password,
->>>>>>> yys
     };
 
     try {
       await instance.post("/Register", userinfo);
-<<<<<<< HEAD
-      console.log(userinfo)
-=======
       console.log(userinfo);
       navigate("/register/verify", { state: userinfo });
->>>>>>> yys
     } catch (error) {
       console.error("Error submitting post:", error);
     }
@@ -111,13 +70,8 @@ function Register() {
           <div className="Idbox">
             <input
               placeholder="  아이디를 입력해주세요"
-<<<<<<< HEAD
-              value={id}
-              onChange={handleIDChange}
-=======
               value={username}
               onChange={handleUsernameChange}
->>>>>>> yys
             ></input>
             <button>중복확인</button>
           </div>
@@ -139,21 +93,6 @@ function Register() {
           <p>비밀번호 확인</p>
           <input
             placeholder="  비밀번호 재입력"
-<<<<<<< HEAD
-            value={checkpassword}
-            onChange={handleCheckChange}
-          ></input>
-        </form>
-        <div>
-          {/* 다음 버튼을 클릭하면 이메일 인증 페이지로 이동 */}
-          <button className="NextBtn" onClick={goToEmailVerification} >
-            다음
-          </button>
-        </div>
-      </div>
-      <div className="Background">
-        <img alt="background"src="img/backphoto.jpg" />
-=======
             value={checkPassword}
             onChange={handleCheckChange}
           ></input>
@@ -167,14 +106,9 @@ function Register() {
       </div>
       <div className="Background">
         <img alt="background" src="img/backphoto.jpg" />
->>>>>>> yys
       </div>
     </div>
   );
 }
 
-<<<<<<< HEAD
 export default Register;
-=======
-export default Register;
->>>>>>> yys
