87e1bedf55322076f89b8410b8b1287aff5f44b8
YeongSeon Yun <dudtjswnstjscoding@gmail.com>
2024-07-05 23:15:11 +0900

    Merge: 수정 해결

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-<<<<<<< HEAD
-import { BrowserRouter } from 'react-router-dom';
-=======
->>>>>>> yys
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<<<<<<< HEAD
-  <BrowserRouter>
     <App />
-  </BrowserRouter>
-=======
-    <App />
->>>>>>> yys
 );
 
