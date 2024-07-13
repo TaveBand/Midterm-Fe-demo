f3d54d921f1da5c7a6961c5533edcce1d7e0710e
YeongSeon Yun <dudtjswnstjscoding@gmail.com>
2024-07-13 23:32:58 +0900

    Merge: Conflict 해결

--- src/App.js
+++ src/App.js
@@ -36,45 +36,41 @@ import UnionPerformances from "./performance/UnionPerformances";
 import UnionPerformanceDetail from "./performance/UnionPerformanceDetail";
 
 function App() {
-  
   return (
     <Router>
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/login" element={<Login />}></Route>
-          <Route path="/register" element={<Register />}></Route>
-          <Route path="/register/verify" element={<Verify />}></Route>
-          <Route path="/register/complete" element={<Complete />}></Route>
-          <Route path="/profile/edit/:user_id" element={<Profile />}></Route>
-          <Route path="/Scrap/:user_id" element={<Scrap />}></Route>
-          <Route path="/MyPosts/:user_id" element={<MyPosts />}></Route>
-          <Route path="/MyPerformances/:user_id" element={<MyPerformances />}></Route>
-          <Route path="/MyReservations/:user_id" element={<MyReservations />}></Route>
-          <Route path="/boards/clubs" element={<Clubs />}></Route>
-          <Route path="/boards/clubs/:post_id" element={<ClubsDetail />}></Route>
-          <Route path="/boards/pr" element={<PR />}></Route>
-          <Route path="/boards/pr/:post_id" element={<PRDetail />}></Route>
-          <Route path="/boards/matching" element={<Matching />}></Route>
-          <Route path="/boards/matching/:post_id" element={<MatchingDetail />}></Route>
-          <Route path="/boards/5" element={<Drum />}></Route>
-          <Route path="/boards/5/:post_id" element={<DrumDetail />}></Route>
-          <Route path="/boards/6" element={<Guitar />}></Route>
-          <Route path="/boards/6/:post_id" element={<GuitarDetail />}></Route>
-          <Route path="/boards/7" element={<Vocal />}></Route>
-          <Route path="/boards/7/:post_id" element={<VocalDetail />}></Route>
-          <Route path="/boards/8" element={<Bass />}></Route>
-          <Route path="/boards/8/:post_id" element={<BassDetail />}></Route>
-          <Route path="/boards/9" element={<Keyboard />}></Route>
-          <Route path="/boards/9/:post_id" element={<KeyboardDetail />}></Route>
-          <Route path="/boards/performances" element={<UnionPerformances />}></Route>
-          <Route path="/boards/performances/:performance_id" element={<UnionPerformanceDetail />}></Route>
-
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/register/verify" element={<Verify />} />
+          <Route path="/register/complete" element={<Complete />} />
+          <Route path="/profile/edit/:user_id" element={<Profile />} />
+          <Route path="/scrap/:user_id" element={<Scrap />} />
+          <Route path="/myposts/:user_id" element={<MyPosts />} />
+          <Route path="/myperformances/:user_id" element={<MyPerformances />} />
+          <Route path="/myreservations/:user_id" element={<MyReservations />} />
+          <Route path="/boards/clubs" element={<Clubs />} />
+          <Route path="/boards/clubs/:post_id" element={<ClubsDetail />} />
+          <Route path="/boards/pr" element={<PR />} />
+          <Route path="/boards/pr/:post_id" element={<PRDetail />} />
+          <Route path="/boards/matching" element={<Matching />} />
+          <Route path="/boards/matching/:post_id" element={<MatchingDetail />} />
+          <Route path="/boards/5" element={<Drum />} />
+          <Route path="/boards/5/:post_id" element={<DrumDetail />} />
+          <Route path="/boards/6" element={<Guitar />} />
+          <Route path="/boards/6/:post_id" element={<GuitarDetail />} />
+          <Route path="/boards/7" element={<Vocal />} />
+          <Route path="/boards/7/:post_id" element={<VocalDetail />} />
+          <Route path="/boards/8" element={<Bass />} />
+          <Route path="/boards/8/:post_id" element={<BassDetail />} />
+          <Route path="/boards/9" element={<Keyboard />} />
+          <Route path="/boards/9/:post_id" element={<KeyboardDetail />} />
+          <Route path="/boards/performances" element={<UnionPerformances />} />
+          <Route path="/boards/performances/:performance_id" element={<UnionPerformanceDetail />} />
         </Routes>
       </AuthProvider>
     </Router>
-
-    
   );
 }
 
